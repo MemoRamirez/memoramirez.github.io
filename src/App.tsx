1268fc15b17931f485f1d65a5cd14d4d37fbd441
Memo Ramirez <memo@memoramirez.com>
2024-07-01 15:03:26 -0500

    added react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Memo Ramirez</h1>
+      <h3>Development Site & Portfolio</h3>
+      <p className='read-the-docs'>Coming Soon</p>
+    </>
+  )
+}
+
+export default App
