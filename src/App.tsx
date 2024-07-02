cdcd29f4c127b861a6a1d750aefe723f7ec6a5a0
Memo Ramirez <memo@memoramirez.com>
2024-07-01 20:51:10 -0500

    added logos and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import memoTsLogo from 'assets/MR_TypeScript.svg'
 
 function App() {
   return (
     <>
-      <h1>Memo Ramirez</h1>
+      <h1 className='logo-title'>
+        <img
+          src={memoTsLogo}
+          className='logo'
+          alt='Memo Ramirez Development logo'
+        />{' '}
+        Memo Ramirez
+      </h1>
       <h3>Development Site & Portfolio</h3>
       <p className='read-the-docs'>Coming Soon</p>
     </>
